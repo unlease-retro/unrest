b647c8e3e1c64c3fc84c1fbe6c3509201b4b85a9
Luke Hedger <luke.hedger@gmail.com>
2016-10-24 12:49:52 +0100

    expose root query for Relay clients

--- server/shared/schema/index.js
+++ server/shared/schema/index.js
@@ -2,9 +2,10 @@ import { GraphQLObjectType, GraphQLSchema } from 'graphql'
 
 import queries from '../queries'
 
+// Query exposes itself as `query` to provide a root query for Relay -> https://github.com/lucasbento/graphql-pokemon/issues/1
 const Query = new GraphQLObjectType({
   name: 'Query',
-  fields: () => ({ ...queries })
+  fields: () => ({ query: { type: Query, resolve: (...args) => args }, ...queries })
 })
 
 export default new GraphQLSchema({
