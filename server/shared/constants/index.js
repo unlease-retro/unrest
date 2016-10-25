2302a32f91c41c9ca60bba359f36e7b3521d36c9
Luke Hedger <luke.hedger@gmail.com>
2016-10-25 12:58:54 +0100

    change API route

--- server/shared/constants/index.js
+++ server/shared/constants/index.js
@@ -6,4 +6,4 @@ export const HOST = process.env.HOST || 'localhost'
 export const PORT = process.env.PORT || '5000'
 export const GRAPHQL_PATH = '/graphql'
 
-export const API = 'https://www.unlease.io/resource'
+export const API = 'https://www.unlease.io'
