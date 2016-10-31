d6585289d07eefa88acc2cbe7c3cf524f46daf6f
Luke Hedger <luke.hedger@gmail.com>
2016-10-31 17:22:06 +0000

    add fields to user type

--- server/user/type.js
+++ server/user/type.js
@@ -1,4 +1,4 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql'
+import { GraphQLObjectType, GraphQLInt, GraphQLString } from 'graphql'
 
 import { name } from './constants'
 import { Type as NotificationType, service as NotificationService } from '../notification'
@@ -7,9 +7,14 @@ const UserType = new GraphQLObjectType({
   name,
   fields: () => ({
     id: { type: GraphQLString },
+    avatar: { type: GraphQLString },
+    email: { type: GraphQLString },
     firstName: { type: GraphQLString },
     lastName: { type: GraphQLString },
-    avatar: { type: GraphQLString },
+    // TODO - can't be INT
+    lastLoggedInAt: { type: GraphQLInt },
+    // TODO - phone = phoneVerification.contactNumber
+    // phoneVerification: { type: GraphQLString },
     notifications: {
       type: NotificationType,
       resolve: (user, args, { token }) => NotificationService.getNotificationById(user.id, token)
