3faf0e9ff8b09aff5503c165c630679f126419ce
Luke Hedger <luke.hedger@gmail.com>
2016-11-01 13:52:38 +0000

    add fields to listing type

--- server/listing/type.js
+++ server/listing/type.js
@@ -1,15 +1,32 @@
-import { GraphQLBoolean, GraphQLObjectType, GraphQLString } from 'graphql'
+import { GraphQLObjectType, GraphQLList, GraphQLBoolean, GraphQLFloat, GraphQLInt, GraphQLString } from 'graphql'
 
 import { name } from './constants'
 import { Type as UserType, service as UserService } from '../user'
+
+const PhotoType = new GraphQLObjectType({
+  name: 'Photo',
+  fields: () => ({
+    s3Link: { type: GraphQLString },
+  })
+})
 
 const ListingType = new GraphQLObjectType({
   name,
   fields: () => ({
     id: { type: GraphQLString },
-    location: { type: GraphQLString },
+    availableFrom: { type: GraphQLFloat },
+    availableTo: { type: GraphQLFloat },
+    createdAt: { type: GraphQLFloat },
     leakage: { type: GraphQLBoolean },
+    location: { type: GraphQLString },
     nonResponsive: { type: GraphQLBoolean },
+    postcode: { type: GraphQLString },
+    title: { type: GraphQLString },
+    weeklyRent: { type: GraphQLInt },
+    photos: {
+      type: new GraphQLList(PhotoType),
+      resolve: listing => listing.imageList
+    },
     user: {
       type: UserType,
       resolve: (listing, args, { token }) => UserService.getUserById(listing.embeddedUser.id, token)
