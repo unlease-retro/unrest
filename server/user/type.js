8bee8df805d6de8990523b4fe0b495da19837b4d
Luke Hedger <luke.hedger@gmail.com>
2016-11-01 10:11:39 +0000

    add phoneVerification.contactNumber to User type

--- server/user/type.js
+++ server/user/type.js
@@ -3,6 +3,13 @@ import { GraphQLObjectType, GraphQLFloat, GraphQLString } from 'graphql'
 import { name } from './constants'
 import { Type as NotificationType, service as NotificationService } from '../notification'
 
+const PhoneVerificationType = new GraphQLObjectType({
+  name: 'PhoneVerification',
+  fields: () => ({
+    contactNumber: { type: GraphQLString },
+  })
+})
+
 const UserType = new GraphQLObjectType({
   name,
   fields: () => ({
@@ -12,6 +19,10 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     lastName: { type: GraphQLString },
     lastLoggedInAt: { type: GraphQLFloat },
+    phoneVerification: {
+      type: PhoneVerificationType,
+      resolve: user => user.phoneVerification
+    },
     notifications: {
       type: NotificationType,
       resolve: (user, args, { token }) => NotificationService.getNotificationById(user.id, token)
