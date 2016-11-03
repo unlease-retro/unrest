4197ff6cefd03879174b613da6beb87b997b3ad1
Luke Hedger <luke.hedger@gmail.com>
2016-11-03 17:49:08 +0000

    update Listing mutation to be Relay compatible

--- server/listing/mutations.js
+++ server/listing/mutations.js
@@ -1,20 +1,27 @@
 import { GraphQLBoolean, GraphQLString } from 'graphql'
+import { mutationWithClientMutationId } from 'graphql-relay'
 
 import Type from './type'
 import * as service from './service'
 
-export const updateHostStatus = {
-  type: Type,
-  args: {
+export const updateHostStatus = mutationWithClientMutationId({
+  name: 'UpdateHostStatus',
+  inputFields: {
     id: { type: GraphQLString },
     leakage: { type: GraphQLBoolean },
     nonResponsive: { type: GraphQLBoolean },
   },
-  resolve: (root, args, { token }) => service.updateHostStatus(token, args).then( json => {
+  outputFields: {
+    listing: {
+      type: Type,
+      resolve: payload => payload
+    }
+  },
+  mutateAndGetPayload: (input, { token }) => service.updateHostStatus(token, input).then( json => {
 
     if (json.error) throw new Error(json.error)
 
     return json
 
   })
-}
+})
--- server/listing/service.js
+++ server/listing/service.js
@@ -4,4 +4,4 @@ import * as API from '../shared/services/api'
 
 export const fetchListings = (token, params) => API.get(`resource/listing/query?${queryString.stringify(params)}`, token)
 
-export const updateHostStatus = (token, params) => API.put('resource/listing/updateField', { name: Object.keys(params).filter( p => p !== 'id' ), listing: params }, token)
+export const updateHostStatus = (token, params) => API.put('resource/listing/updateField', { name: Object.keys(params).filter( p => p !== 'id' && p !== 'clientMutationId' ), listing: params }, token)
