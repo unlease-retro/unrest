52e5eca630f3a82532a9b92c7b2bf5da814887a6
Luke Hedger <luke.hedger@gmail.com>
2016-11-10 15:58:01 +0000

    use new API constant in Auth service

--- server/auth/service.js
+++ server/auth/service.js
@@ -1,6 +1,7 @@
+import { API_UNLEASE } from '../shared/constants'
 import * as API from '../shared/services/api'
 import { getBasicAuth } from '../shared/util'
 
-export const login = ({ username, password }) => API.postForm('uaa/oauth/token', { grant_type: 'password', username, password }, `Basic ${getBasicAuth()}`)
+export const login = ({ username, password }) => API.postForm(`${API_UNLEASE}/uaa/oauth/token`, { grant_type: 'password', username, password }, `Basic ${getBasicAuth()}`)
 
-export const refresh = ({ refresh_token }) => API.postForm('uaa/oauth/token', { grant_type: 'refresh_token', refresh_token }, `Basic ${getBasicAuth()}`)
+export const refresh = ({ refresh_token }) => API.postForm(`${API_UNLEASE}/uaa/oauth/token`, { grant_type: 'refresh_token', refresh_token }, `Basic ${getBasicAuth()}`)
