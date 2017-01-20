2a8a3edf1cd54394247af690b914e9df71b5b3b9
Luke Hedger <luke.hedger@gmail.com>
2017-01-20 17:19:41 +0000

    revert to new staging db

--- server/bot/constants.js
+++ server/bot/constants.js
@@ -18,6 +18,6 @@ export const adverts = 'adverts'
 
 export const replies = 'replies'
 
-export const dbname = isDevelopment ? 'optimus' : 'bot_prod'
+export const dbname = isDevelopment ? 'bot-test' : 'bot_prod'
 
-export const MONGO_URL = isDevelopment ? 'mongodb://admin:Unlease12#@ds049496.mlab.com:49496/optimus' : 'mongodb://unlease_user:1984Izzy@ds019499-a0.klj37.fleet.mlab.com:19499,ds019499-a1.klj37.fleet.mlab.com:19499/bot_prod?replicaSet=rs-ds019499&ssl=true'
+export const MONGO_URL = isDevelopment ? 'mongodb://test-user:1984Izzy@ds029575.mlab.com:29575/bot-test' : 'mongodb://unlease_user:1984Izzy@ds019499-a0.klj37.fleet.mlab.com:19499,ds019499-a1.klj37.fleet.mlab.com:19499/bot_prod?replicaSet=rs-ds019499&ssl=true'
