3938c8c5060f643e6382454657a03dd59619c053
Luke Hedger <luke.hedger@gmail.com>
2017-01-24 14:52:14 +0000

    make listed true!

--- server/unlease/listing/service.js
+++ server/unlease/listing/service.js
@@ -35,7 +35,7 @@ export const createUserWithListing = (token, { listing, user}) => {
 
   return UserService.createUser(token, user)
     .then( () => createListing(token, listing) )
-    .then( listing => uploadImages(listing.id, imageList).then( imageList => updateListing(token, { ...listing, photo: { imageList: imageList.map( ({ s3Link }) => ({ s3Link, name: `${IMAGE_NAME}-${uuid.v4()}${path.extname(s3Link)}` })) } }) ) )
+    .then( listing => uploadImages(listing.id, imageList).then( imageList => updateListing(token, { ...listing, photo: { imageList: imageList.map( ({ s3Link }) => ({ s3Link, name: `${IMAGE_NAME}-${uuid.v4()}${path.extname(s3Link)}` })), sectionCompleted: true } }) ) )
     .then( listing => createBotListing(token, { listingId: listing.id }))
 
 }
