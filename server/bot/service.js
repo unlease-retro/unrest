dbd8ab790211dcb39afec552975df9eaa24991d1
Marius Balaj <marius@Mariuss-Mac-mini.local>
2017-01-31 16:34:24 +0200

    [ BOT ] - id -> _id 🔨

--- server/bot/service.js
+++ server/bot/service.js
@@ -3,9 +3,9 @@ import { adverts, replies } from './constants'
 
 export const allAdverts = ({ submitted, disabled, crawled, limit }, db) => db.collection( adverts ).find( { submitted, disabled, crawled } ).limit( limit ).toArray()
 
-export const advert = ({ id }, db) => db.collection( adverts ).findOne( { _id: ObjectID( id ) } )
+export const advert = ({ _id }, db) => db.collection( adverts ).findOne( { _id: ObjectID( _id ) } )
 
-export const updateAdvert = ({ id, payload }, db) => db.collection( adverts ).findOneAndUpdate({ _id: ObjectID(id) }, { $set: { ...payload } }, { returnOriginal: false })
+export const updateAdvert = ({ _id, payload }, db) => db.collection( adverts ).findOneAndUpdate({ _id: ObjectID(_id) }, { $set: { ...payload } }, { returnOriginal: false })
 
 export const createAdvert = ({ payload }, db) => db.collection( adverts ).insert( payload )
 
