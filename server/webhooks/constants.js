906af5ea35d51306bb4a8834bb984267f5ba8b04
Izzy <xinjin214@gmail.com>
2017-02-24 11:34:30 +0000

    [BOT] use #testing channel

--- server/webhooks/constants.js
+++ server/webhooks/constants.js
@@ -2,7 +2,7 @@ import { isDevelopment } from '../shared/util'
 
 export const name = 'webhooks'
 
-export const SLACK_CHANNEL = isDevelopment ? '#testing1' : '#botresponse'
+export const SLACK_CHANNEL = isDevelopment ? '#testing' : '#botresponse'
 export const SLACK_ICON_EMOJI = ':unlease:'
 export const SLACK_USERNAME = 'unlease'
 export const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'
